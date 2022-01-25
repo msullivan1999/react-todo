f7d474b12ee3d99afc64106537288052e3d1a20d
Matt Sullivan <matt.r.sullivan1999@gmail.com>
2022-01-25 21:12:56 +1100

    remove button shadows

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -18,7 +18,7 @@ export default class addNote extends Component {
     render() {
         return (
             <div>
-                <button className='btn btn-warning btn-md m-5' onClick={this.createNote()}>Add Note</button>
+                <button className='btn btn-warning btn-md m-5 shadow-none' onClick={this.createNote()}>Add Note</button>
             </div>
         )
     }
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -26,8 +26,8 @@ export default class note extends Component {
                                         </p>
                                     </div>
                                     <div className="d-flex justify-content-between">
-                                        <button className="btn btn-primary btn-sm mb-2">Edit</button>
-                                        <button className="btn btn-danger btn-sm mb-2"><img src={trash} alt="delete"></img></button>
+                                        <button className="btn btn-primary btn-sm mb-2 shadow-none">Edit</button>
+                                        <button className="btn btn-danger btn-sm mb-2 shadow-none"><img src={trash} alt="delete"></img></button>
                                     </div>
                                 </div>
                         )
