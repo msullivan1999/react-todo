a9a01dd7918129bdcdb27ceed4e0094aebb7abb8
Matt Sullivan <matt.r.sullivan1999@gmail.com>
2022-01-25 16:32:43 +1100

    add bootstrap

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -4,7 +4,7 @@ export default class addNote extends Component {
     render() {
         return (
             <div>
-                <h1>this will be the component to add a new new</h1>
+                <button className='btn btn-primary'>Add Note</button>
             </div>
         )
     }
