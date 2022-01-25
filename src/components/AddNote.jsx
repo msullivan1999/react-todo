c02ab7a0c208c773190bfdf0ed1b2965f329123e
Matt Sullivan <msul3031@uni.sydney.edu.au>
2022-01-25 16:21:36 +1100

    set up components and check rendering

---
+++ src/components/AddNote.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class addNote extends Component {
+    render() {
+        return (
+            <div>
+                <h1>this will be the component to add a new new</h1>
+            </div>
+        )
+    }
+}
---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class note extends Component {
+    render() {
+        return (
+            <div>
+                <h2>This will be all of our notes</h2> 
+            </div>
+        )
+    }
+}
